cd414a42619eba4bc6259ba62bbb9a9a8516dc71
Xiaoying Qu <qux@carleton.edu>
2023-12-11 10:12:30 +0100

    Update ResultsScreen.kt

--- app/src/main/java/hu/ait/restauright/screen/results/ResultsScreen.kt
+++ app/src/main/java/hu/ait/restauright/screen/results/ResultsScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,6 +24,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import hu.ait.restauright.screen.login.ResultsUiState
 import hu.ait.restauright.screen.login.ResultsViewModel
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.text.AnnotatedString
@@ -32,10 +35,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.graphics.Color
+import coil.compose.rememberAsyncImagePainter
+import hu.ait.restauright.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ResultsScreen (
+fun ResultsScreen(
     modifier: Modifier = Modifier,
     sessionId: String,
     resultsViewModel: ResultsViewModel = viewModel()
@@ -55,12 +61,24 @@ fun ResultsScreen (
     ) {
         TopAppBar(
             title = {
-                Text(
-                    text = "Results",
-                    modifier = Modifier.padding(16.dp)
-                )
-
-            },
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        painter = rememberAsyncImagePainter(R.drawable.logo),
+                        contentDescription = "Logo Image",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.padding(end = 8.dp)
+                            .size(45.dp)
+                    )
+                    Text(
+                        text = "Results",
+                        modifier = Modifier.padding(16.dp)
+                            .size(45.dp)
+                    )
+                }
+            }
         )
 
         LazyColumn(
